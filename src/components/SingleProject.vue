d3c82421a3a55b954b338ce37d9df4608f311cbc
mohsenrs <mohsen8022@gmail.com>
2021-02-18 13:32:36 +0330

    done tick

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
       <div class="icons">
         <span class="material-icons">edit</span>
         <span @click="deleteProject" class="material-icons">delete</span>
-        <span class="material-icons">done</span>
+        <span @click="toggleComplete" class="material-icons tick">done</span>
       </div>
     </div>
     <div class="details" v-if="showDetails">
@@ -20,16 +20,25 @@ export default {
   data() {
     return {
       showDetails: false,
-      uri: "http://localhost:3000/projects" + this.project.id,
+      uri: "http://localhost:3000/projects/" + this.project.id,
     };
   },
   methods: {
     deleteProject() {
       fetch("this.uri", { method: "DELETE" }).then(() =>
-        this.$emit("delete", this.project.id).catch((err) =>
-          console.log(err)
-        )
+        this.$emit("delete", this.project.id).catch((err) => console.log(err))
       );
+    },
+    toggleComplete() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ complete: !this.project.complete }),
+      })
+        .then(() => {
+          this.$emit("complete", this.project.id);
+        })
+        .catch((err) => console.log(err));
     },
   },
 };
@@ -54,6 +63,12 @@ export default {
 }
 .material-icons:hover {
   color: darkgray;
+}
+.project.complete {
+  border-left: 4px solid #00e961;
+}
+.project.complete .tick {
+  color:  #00e961;
 }
 h3 {
   cursor: pointer;
